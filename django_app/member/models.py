963b29f27a565859237383d3e3fbaa1fd3d78963
Joe Donghee Cho <recordingbetter@gmail.com>
2017-07-06 22:00:19 +0900

    modify member.models

--- django_app/member/models.py
+++ django_app/member/models.py
@@ -4,3 +4,6 @@ from django.db import models
 
 class MyUser(AbstractUser):
     img_profile = models.ImageField(upload_to='user', blank=True)
+    nickname = models.CharField(max_length=50)
+    slack = models.CharField(max_length=50)
+    phone = models.CharField(max_length=15)
