9763f90047bf9ac9b0da40e9234d0849eb07e176
Joe Donghee Cho <recordingbetter@gmail.com>
2017-07-07 03:09:50 +0900

    modify config.urls

--- django_app/config/urls.py
+++ django_app/config/urls.py
@@ -13,7 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.conf.urls import url, include
+from django.conf.urls.static import static
 from django.contrib import admin
 
 from member import views
@@ -24,3 +26,7 @@ urlpatterns = [
     url(r'^member/', include('member.urls')),
     url(r'^$', views.login)
 ]
+# /static/에 대한 요청을 STATIC_ROOT경로의 파일에서 찾는다
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# /media/에 대한 요청을 MEDIA_ROOT경로의 파일에서 찾는다
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
