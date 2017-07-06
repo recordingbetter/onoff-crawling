9e81dc0d4b22210f53bcd89e216c4ec3b24a9188
Joe Donghee Cho <recordingbetter@gmail.com>
2017-07-06 21:05:41 +0900

    modify models

--- django_app/crawling/models.py
+++ django_app/crawling/models.py
@@ -1,20 +1,17 @@
-from django.contrib.auth.models import AbstractUser
+
 from django.db import models
-
-
-class MyUser(AbstractUser):
-    img_profile = models.ImageField(upload_to='user', blank=True)
-
-
-class News(models.Model):
-    user = models.ForeignKey(MyUser)
-    title = models.CharField(max_length=100)
-    detail_link = models.CharField(max_length=200)
-    img_news = models.ImageField(upload_to='news', blank=True)
-    created_date = models.DateTimeField(auto_now_add=True)
+from member.models import MyUser
 
 
 class Keyword(models.Model):
-    user = models.ForeignKey(MyUser)
+    user = models.ForeignKey(MyUser, on_delete=models.CASCADE)
     text = models.CharField(max_length=50)
+    created_date = models.DateTimeField(auto_now_add=True)
+
+
+class News(models.Model):
+    user = models.ForeignKey(MyUser, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    detail_link = models.CharField(max_length=200)
+    img_news = models.ImageField(upload_to='news', blank=True)
     created_date = models.DateTimeField(auto_now_add=True)
